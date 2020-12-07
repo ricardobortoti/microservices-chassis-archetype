c27a9010ef849179c1ef6ad0a9e136fe96eff4f5
ricardobortoti <ricardobortoti@gmail.com>
2020-12-06 22:58:45 -0300

    fixing cors configuration

--- src/main/resources/archetype-resources/src/main/java/__project-name-lowercase__/configuration/JwtAuthenticationConfig.java
+++ src/main/resources/archetype-resources/src/main/java/__project-name-lowercase__/configuration/JwtAuthenticationConfig.java
@@ -1,6 +1,6 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
+#set( $symbol_pound='#')
+#set( $symbol_dollar='$')
+#set( $symbol_escape='\' )
 package ${package}.${project-name-lowercase}.configuration;
 
 import org.springframework.context.annotation.Configuration;
@@ -10,8 +10,8 @@ import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
